7c8663be4c334be80639da3bfd058d3210811df2
zouz82 <jagal_zouzou@hotmail.com>
2021-10-29 21:12:55 -0400

    Added a combo with every move and found a way to only display ename

--- main.py
+++ main.py
@@ -3,29 +3,30 @@ import json
 from Move import Move
 from Pokemon import Pokemon
 
+
 def jsonToMoves():
     movesList = []
 
-    ##with open('json/moves.json') as moves:
-      ##  moveObj = json.dumps(str(moves))
-        ##movesList.append(Move(moveObj.index("e"), moveObj.index("t"), moveObj.index("p"), moveObj.index("c")))
-    for moves in open('json/moves.json'):
-        moveObj = json.dumps(str(moves))
-        movesList.append(Move(moveObj.index("e"), moveObj.index("t"), moveObj.index("p"), moveObj.index("c")))
+    with open('json/moves.json') as moves:
+        # for moveObj in moves:
+        # moveObj = moves.read()
+        moveDict = json.load(moves)
+        for i in moveDict:
+            movesList.append(i)
+
     return movesList
 
 
+layout = [[sg.Text("test")], [sg.Combo(jsonToMoves(), key='moveCombo')], [sg.Button("Info")]]
 
-layout = [[sg.Text("test")], [sg.DropDown([str(jsonToMoves()[0])])], [sg.Button("Info")]]
+# pokemon = Pokemon(1, [layout.__getitem__(1), layout.__getitem__(2), layout.__getitem__(3), layout.__getitem__(4)], 1, 1, 1, 1, "Fire")
 
-#pokemon = Pokemon(1, [layout.__getitem__(1), layout.__getitem__(2), layout.__getitem__(3), layout.__getitem__(4)], 1, 1, 1, 1, "Fire")
-
-window = sg.Window('testTitle', layout)
+window = sg.Window('Pokelutor', layout)
 
 event, values = window.read()
 
-if(event == 'Info'):
-    print(str(jsonToMoves()[0]))
-
+if (event == 'Info'):
+    #key is to call a specific component directly
+    print(values['moveCombo']['ename'])
 
 window.close()
